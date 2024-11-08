563fd02b9e0db211ca0e5797ed4d94f248bd16b5
candi4 <hojun327@naver.com>
2024-11-08 23:34:54 +0900

    Saved transform_matrix into yaml

--- render.py
+++ render.py
@@ -14,7 +14,6 @@ voxel_size = params["voxel_size"]
 object_filenames = params['object_filenames']
 assembly_calibrations = params['assembly_calibrations']
 
-
 # The center of the larger circle of part11 is the origin of the assembly.
 # part frame based on assembly frame (a_i)
 for part, matrix in assembly_calibrations.items():
@@ -32,17 +31,12 @@ assert set(object_filenames).issubset(set(assembly_calibrations.keys())), f"asse
 for ry in range(-90,91,10):
     for rx in range(-90,91,10):
         delx = 0.1 # delx mm for one pixel
+        cycle_start = time.time()
         # Transform the assembly based on the world frame
         # assembly frame based on world frame  (w_a)
-        H_wc = torch.tensor([[1,0,0,0], # in mm
-                                [0,1,0,0],
-                                [0,0,1,950], # z < sdd
-                                [0,0,0,1]], dtype=float)
+        H_wc = torch.tensor(params['transform_matrix']['H_wc'], dtype=float)
         H_wc[:3,:3] = torch.tensor(R.from_euler('yx',(ry,rx), True).as_matrix())
-        H_ca = torch.tensor([[1,0,0,0],
-                                [0,1,0,-45/2],
-                                [0,0,1,0],
-                                [0,0,0,1]], dtype=float)
+        H_ca = torch.tensor(params['transform_matrix']['H_ca'], dtype=float)
         transform_matrix = H_wc @ H_ca
         image_nps = dict()
         for object_filename in object_filenames:
@@ -61,5 +55,6 @@ for ry in range(-90,91,10):
             assembly_image += image_nps[object_filename]
         assembly_image = crop_nonzero(assembly_image)
         save_image(image_np=assembly_image, image_filename=f'images/module/(ry{ry})(rx{rx})(delx{delx}).png', pixel_max=assembly_image.max(), printing=True)
+        print("    One cycle time:", time.time()-cycle_start)
 
 print("Consumed time:",time.time()-start_time)
