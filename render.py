c7b448f252aac2648fe89c16adae6b27d4a42ecf
candi4 <hojun327@naver.com>
2024-11-24 19:20:00 +0900

    Narrowed range of angle

--- render.py
+++ render.py
@@ -27,7 +27,7 @@ for i in (pbar := tqdm(range(1000), desc="Render")):
     if i == 0: rx=ry=0
     else:
         ry = 0 #random.uniform(0,360)
-        rx = random.uniform(-40,40)
+        rx = random.uniform(-20,20)
     delx = 0.1 # delx mm for one pixel
     image_filename=f'images/module/ry{int(ry):03}_rx{int(rx):+03}_{random.randint(0,9999):04}.png'
     pbar.set_postfix(image_filename=os.path.split(image_filename)[-1])
